df45c68f7ac698960a9c1e67394f7deb14e448e8
Emily Millard <emily_millard@live.co.uk>
2021-11-09 16:00:53 +0100

    feat: update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,13 +14,26 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to edit_task_path
+      redirect_to tasks_path
     end
   end
 
   def edit
     @task = Task.find(params[:id])
-    redirect_to tasks_path
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    if @task.destroy
+      redirect_to tasks_path
+    end
   end
 
   private
@@ -28,5 +41,4 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:title, :details)
   end
-
 end
